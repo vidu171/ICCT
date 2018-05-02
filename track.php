d4fb421475c57057675cb5bf69d0877a48df9921
tusharsadana <tushar.169107181@muj.manipal.edu>
2018-05-03 00:19:21 +0530

    Tracks corrected

--- track.php
+++ track.php
@@ -17,19 +17,9 @@
          ?>
       <div class="vc_custom_heading" style="margin-bottom:1em">
          <h2 style="text-align: center;font-family:Lato;font-weight:900;font-style:normal;letter-spacing:2pt" >ICCT 19 Tracks</h2>
-         <div class="line_bottom_heading" style="width:150px;
-            height:3px;
-            margin:30px 0 0 0;
-            background:#ffbf00;
-            position: relative;
-            bottom: 0;
-            left: 50%;
-            -webkit-transform: translateX(-50%);
-            -khtml-transform: translateX(-50%);
-            -moz-transform: translateX(-50%);
-            -ms-transform: translateX(-50%);
-            -o-transform: translateX(-50%);
-            transform: translateX(-50%);"></div>
+         <div class="line_bottom_heading" style="width:150px;height:3px;margin:30px 0 0 0;background:#ffbf00;position: relative;bottom: 0;left: 50%;-webkit-transform: translateX(-50%);-khtml-transform: translateX(-50%);-moz-transform: translateX(-50%);-ms-transform: translateX(-50%);-o-transform: translateX(-50%);transform: translateX(-50%);">
+
+         </div>
       </div>
       <div  class="secsion clearfix "  style ="">
          <div class="container-fluid clearfix">
@@ -37,37 +27,24 @@
                <div class="vc_col-sm-6 wpb_column vc_column_container  vc_custom_1441867091350   " style="">
                   <div class="vc_column-inner clearfix vc_custom_1441867091350" style="padding-right:0px;padding-left:0px">
                      <div class="wrapper-content">
-                        <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout9 content-align-center  " id="cms-fancy-box-single-2"
-                           >
+                        <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout9 content-align-center  " id="cms-fancy-box-single-2">
                            <div class="cms-fancyboxes-body">
                               <div class="cms-fancybox-item">
                                  <div class="fancy-box-image">
                                     <img class="img-responsive" src="temp/left.jpg" alt="" />
                                  </div>
-                                 <div class="fancybox-primary" style="
-                                    background:#fff;
-                                    padding:10% 8% 8%;
-                                    ">
-                                    <h3 class="facybox-title" style="
-                                       font-size:35px;
-                                       ">
-                                       STOCK PHOTOGRAPHY                                                    <span class="line-title" style="
-                                          background: #ffbf00;
-                                          width: 100px;
-                                          height: 3px;
-                                          margin: 22px auto 27px;
-                                          "></span>
+                                 <div class="fancybox-primary" style="background:#fff;padding:10% 8% 8%;">
+                                    <h3 class="facybox-title" style="font-size:28px;">
+                                       TRACK1: INTELLIGENT COMMUNICATION
+                                        <span class="line-title" style="background: #ffbf00;width: 100px;height: 3px;margin: 22px auto 27px;"></span>
                                     </h3>
-                                    <div class="fancy-box-content" style="
-                                       color:#999999;
-                                       ">
-                                       <p>Developing a diverse and robust portfolio is essential, since selling microstock is a numbers game. </p>
+                                    <div class="fancy-box-content" style="color:#999999;">
+                                       <p> Developing a diverse and robust portfolio is essential, since selling microstock is a numbers game. </p>
                                     </div>
                                     <div class="cms-fancyboxes-foot">
-                                       <a href="#" class="wpb_button_anone wpb_none btn btn-default cms-bounce-to-bottom"
-                                          style="
-                                          ">
-                                       MORE INFO                        </a>
+                                       <a href="#" class="wpb_button_anone wpb_none btn btn-default cms-bounce-to-bottom" style="">
+                                       MORE INFO
+                                     </a>
                                     </div>
                                  </div>
                               </div>
@@ -76,41 +53,27 @@
                      </div>
                   </div>
                </div>
-               <div class="vc_col-sm-6 wpb_column vc_column_container  vc_custom_1441867110004   " style="">
+               <div class="vc_col-sm-6 wpb_column vc_column_container  vc_custom_1441867110004" style="">
                   <div class="vc_column-inner clearfix vc_custom_1441867110004">
                      <div class="wrapper-content">
-                        <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout9 content-align-center  " id="cms-fancy-box-single-3"
-                           >
+                        <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout9 content-align-center" id="cms-fancy-box-single-3">
                            <div class="cms-fancyboxes-body">
                               <div class="cms-fancybox-item">
                                  <div class="fancy-box-image">
                                     <img class="img-responsive" src="temp/right.jpg" alt="" />
                                  </div>
-                                 <div class="fancybox-primary" style="
-                                    background:#ffbf00;
-                                    padding:10% 8% 8%;
-                                    ">
-                                    <h3 class="facybox-title" style="
-                                       font-size:35px;
-                                       ">
-                                       EDITORIAL WORK                                                    <span class="line-title" style="
-                                          background: #000000;
-                                          width: 100px;
-                                          height: 3px;
-                                          margin: 22px auto 27px;
-                                          "></span>
+                                 <div class="fancybox-primary" style="background:#ffbf00;padding:10% 8% 8%;">
+                                    <h3 class="facybox-title" style="font-size:28px;">
+                                       EDITORIAL WORK
+                                        <span class="line-title" style="background: #000000; width: 100px; height: 3px;margin: 22px auto 27px;"></span>
                                     </h3>
-                                    <div class="fancy-box-content" style="
-                                       color:#000000;
-                                       ">
+                                    <div class="fancy-box-content" style="color:#000000;">
                                        <p>Editorial work can be varied, a challenge to do successfully, but they&#8217;re rewarding and lead to meeting people along the way.</p>
                                     </div>
                                     <div class="cms-fancyboxes-foot">
-                                       <a href="#" class="wpb_button_anone wpb_none btn btn-default cms-bounce-to-bottom"
-                                          style="
-                                          background:rgba(0,0,0,0.01);
-                                          ">
-                                       MORE INFO                        </a>
+                                       <a href="#" class="wpb_button_anone wpb_none btn btn-default cms-bounce-to-bottom"  style="background:rgba(0,0,0,0.01);">
+                                       MORE INFO
+                                     </a>
                                     </div>
                                  </div>
                               </div>
@@ -128,33 +91,20 @@
                <div class="vc_col-sm-6 vc_col-lg-3 vc_col-md-3 wpb_column vc_column_container  vc_custom_1441881119709   " style="">
                   <div class="vc_column-inner clearfix vc_custom_1441881119709">
                      <div class="wrapper-content">
-                        <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout10 content-align-center " id="cms-fancy-box-single-4"
-                           style="
-                           padding:15% 5%;
-                           " onclick="">
+                        <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout10 content-align-center " id="cms-fancy-box-single-4"  style="padding:15% 5%;" onclick="">
                            <div class="cms-fancyboxes-body">
                               <div class="cms-fancybox-item">
                                  <div class="fancy-box-icon">
                                     <span class="icon" style="border: 2px solid #000000;">
-                                    <i style="
-                                       color:#000000;
-                                       font-size:60px;
-                                       " class="pe-7s-leaf"></i>
+                                    <i style="color:#000000; font-size:60px;" class="pe-7s-leaf"></i>
                                     <span class="overlay" style=""></span>
                                     </span>
                                  </div>
-                                 <h4 class="facybox-title" style="
-                                    ">
-                                    COOL PORTFOLIO LAYOUTS                                            <span class="line-title" style="
-                                       background: #ffbf00;
-                                       width: 100px;
-                                       height: 3px;
-                                       margin: 30px auto;
-                                       "></span>
+                                 <h4 class="facybox-title" style="">
+                                    COOL PORTFOLIO LAYOUTS
+                                       <span class="line-title" style="background: #ffbf00;  width: 100px;  height: 3px; margin: 30px auto;"></span>
                                  </h4>
-                                 <div class="fancy-box-content" style="
-                                    color:#999999;
-                                    ">
+                                 <div class="fancy-box-content" style="color:#999999; ">
                                     <p>Bolina can attract customers by organizing a good portfolio. it is interesting because of already creating logical content to match with users’ demand.</p>
                                  </div>
                               </div>
@@ -163,36 +113,24 @@
                      </div>
                   </div>
                </div>
-               <div class="vc_col-sm-6 vc_col-lg-3 vc_col-md-3 wpb_column vc_column_container  vc_custom_1441881126004   " style="">
+               <div class="vc_col-sm-6 vc_col-lg-3 vc_col-md-3 wpb_column vc_column_container  vc_custom_1441881126004 " style="">
                   <div class="vc_column-inner clearfix vc_custom_1441881126004">
                      <div class="wrapper-content">
                         <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout10 content-align-center " id="cms-fancy-box-single-5"
-                           style="
-                           padding:15% 5%;
-                           " onclick="">
+                           style="padding:15% 5%;" onclick="">
                            <div class="cms-fancyboxes-body">
                               <div class="cms-fancybox-item">
                                  <div class="fancy-box-icon">
                                     <span class="icon" style="border: 2px solid #000000;">
-                                    <i style="
-                                       color:#000000;
-                                       font-size:60px;
-                                       " class="pe-7s-paint-bucket"></i>
+                                    <i style="color:#000000; font-size:60px;" class="pe-7s-paint-bucket"></i>
                                     <span class="overlay" style=""></span>
                                     </span>
                                  </div>
-                                 <h4 class="facybox-title" style="
-                                    ">
-                                    DEVELOPER PLATFORM                                            <span class="line-title" style="
-                                       background: #ffbf00;
-                                       width: 100px;
-                                       height: 3px;
-                                       margin: 30px auto;
-                                       "></span>
+                                 <h4 class="facybox-title" style=" ">
+                                    DEVELOPER PLATFORM
+                                     <span class="line-title" style=" background: #ffbf00;  width: 100px;height: 3px;  margin: 30px auto;  "></span>
                                  </h4>
-                                 <div class="fancy-box-content" style="
-                                    color:#999999;
-                                    ">
+                                 <div class="fancy-box-content" style=" color:#999999; ">
                                     <p>A great characteristic of this theme is to help users customizing the code and building a platform followed by demands. It is suitable for customers’ purpose.</p>
                                  </div>
                               </div>
@@ -205,32 +143,20 @@
                   <div class="vc_column-inner clearfix vc_custom_1441881132837">
                      <div class="wrapper-content">
                         <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout10 content-align-center " id="cms-fancy-box-single-6"
-                           style="
-                           padding:15% 5%;
-                           " onclick="">
+                           style="  padding:15% 5%; " onclick="">
                            <div class="cms-fancyboxes-body">
                               <div class="cms-fancybox-item">
                                  <div class="fancy-box-icon">
                                     <span class="icon" style="border: 2px solid #000000;">
-                                    <i style="
-                                       color:#000000;
-                                       font-size:60px;
-                                       " class="pe-7s-rocket"></i>
+                                    <i style=" color:#000000;   font-size:60px; " class="pe-7s-rocket"></i>
                                     <span class="overlay" style=""></span>
                                     </span>
                                  </div>
-                                 <h4 class="facybox-title" style="
-                                    ">
-                                    COOL PORTFOLIO LAYOUTS                                            <span class="line-title" style="
-                                       background: #ffbf00;
-                                       width: 100px;
-                                       height: 3px;
-                                       margin: 30px auto;
-                                       "></span>
+                                 <h4 class="facybox-title" style="  ">
+                                    COOL PORTFOLIO LAYOUTS
+                                      <span class="line-title" style=" background: #ffbf00;  width: 100px;  height: 3px; margin: 30px auto;"></span>
                                  </h4>
-                                 <div class="fancy-box-content" style="
-                                    color:#999999;
-                                    ">
+                                 <div class="fancy-box-content" style="color:#999999;  ">
                                     <p>Bolina can attract customers by organizing a good portfolio. it is interesting because of already creating logical content to match with users’ demand.</p>
                                  </div>
                               </div>
@@ -243,32 +169,20 @@
                   <div class="vc_column-inner clearfix vc_custom_1441881140365">
                      <div class="wrapper-content">
                         <div class="cms-fancyboxes-wraper template-cms_fancybox_single--layout10 content-align-center " id="cms-fancy-box-single-7"
-                           style="
-                           padding:15% 5%;
-                           " onclick="">
+                           style="padding:15% 5%; " onclick="">
                            <div class="cms-fancyboxes-body">
                               <div class="cms-fancybox-item">
                                  <div class="fancy-box-icon">
                                     <span class="icon" style="border: 2px solid #000000;">
-                                    <i style="
-                                       color:#000000;
-                                       font-size:60px;
-                                       " class="pe-7s-helm"></i>
+                                    <i style="  color:#000000;   font-size:60px; " class="pe-7s-helm"></i>
                                     <span class="overlay" style=""></span>
                                     </span>
                                  </div>
-                                 <h4 class="facybox-title" style="
-                                    ">
-                                    GOOGLE FONT COLLECTION                                            <span class="line-title" style="
-                                       background: #ffbf00;
-                                       width: 100px;
-                                       height: 3px;
-                                       margin: 30px auto;
-                                       "></span>
+                                 <h4 class="facybox-title" style=" ">
+                                    GOOGLE FONT COLLECTION
+                                     <span class="line-title" style=" background: #ffbf00;  width: 100px;   height: 3px;   margin: 30px auto;  "></span>
                                  </h4>
-                                 <div class="fancy-box-content" style="
-                                    color:#999999;
-                                    ">
+                                 <div class="fancy-box-content" style=" color:#999999;">
                                     <p>Google Font Collection which is a great feature set up in Bolina. Users can be convenient to build content with variable fonts to make attractive</p>
                                  </div>
                               </div>
