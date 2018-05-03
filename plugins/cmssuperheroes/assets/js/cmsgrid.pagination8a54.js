6920416960014d6e1f6805352ed1808d0ee4e2af
aesher9o1 <aashiskumar986@gmail.com>
2018-05-03 12:52:16 +0530

    Aashis branch initiated

--- plugins/cmssuperheroes/assets/js/cmsgrid.pagination8a54.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(a){"use strict";a(".cms-grid-wraper").each(function(){var b=a(this),c=a(this).attr("id");b.find("a.page-numbers").live("click",function(){b.fadeTo("slow",.3);var d=a(this).attr("href");return jQuery.get(d,function(d){b.html(a(d).find("#"+c).html()),b.fadeTo("slow",1)}),jQuery("audio,video").mediaelementplayer(),!1})})});
